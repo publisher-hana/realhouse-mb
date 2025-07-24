c6a8fb544208a3caa829141e8c20c0ca3c9f6c3b
lhn <hanaya1208@gmail.com>
2025-07-24 16:14:03 +0900

    업로드

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,34 @@
+import { Link } from "react-router-dom"
+import './commonlayout.css'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <nav className="bottom-nav">
+        <Link className="nav-item active">
+          <span className="icon">🏠</span>
+          <span className="label">홈</span>
+        </Link>
+        <Link className="nav-item">
+          <span className="icon">📍</span>
+          <span className="label">지도</span>
+        </Link>
+        <Link className="nav-item">
+          <span className="icon">🏢</span>
+          <span className="label">중개현황</span>
+        </Link>
+        <Link className="nav-item">
+          <span className="icon badge-container">
+            🔔
+            <span className="badge">4</span>
+          </span>
+          <span className="label">알림</span>
+        </Link>
+        <Link className="nav-item">
+          <span className="icon">⋯</span>
+          <span className="label">더보기</span>
+        </Link>
+      </nav>
+    </>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
+import './homepage.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="main">
+      <h1 className="main-tit">헛걸음은 없다 <p>허위매물이 근본적으로<br />차단되는 하우스!</p></h1>
+    </div>
   )
 }
 
